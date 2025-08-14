c6d74ee9dc7f758dcb37819ee8a9d8b4ddecedda
Md Irfan <work.mdirfan@gmail.com>
2025-08-14 18:53:50 +0530

    Github profile project

---
+++ WorkMdirfan29_CodeSpaces/02_githubuser_profile/src/components/card/Card.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div className='wrapper'>
+        <div className='cardcontainer'>
+            <div className="leftcontainer">
+                <img src="" alt="" />
+                <h1>Name:</h1>
+                <h2>Username:</h2>
+                <p>Bio:</p>
+            </div>
+            <div className="rightcontainer">
+                <img src="" alt="" />
+                <img src="" alt="" />
+                <img src="" alt="" />
+            <div className="downcontainer">
+                <img src="" alt="" />
+                <img src="" alt="" />
+                <img src="" alt="" />
+                <img src="" alt="" />
+                <img src="" alt="" />
+                <img src="" alt="" />
+            </div>
+
+            </div>
+        </div>
+
+    </div>
+  )
+}
+
+export default Card
